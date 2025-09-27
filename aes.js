c02d0b2408278e925f4955cbc215b927d06f26f4
Samir Geronimo Zeta Benites <szetab@ucvvirtual.edu.pe>
2025-09-26 21:13:44 -0500

    Version Final

--- aes.js
+++ aes.js
@@ -1,21 +1,38 @@
 const clave = "claveaes12345678"; // Clave de 128 bits
 
 function encriptar() {
-    const texto = document.getElementById("texto").value;
-    const resultado = CryptoJS.AES.encrypt(texto, clave).toString();
-    document.getElementById("resultado").value = resultado;
+    const textarea = document.getElementById("texto");
+    const texto = textarea.value.trim(); // eliminamos espacios en blanco
+    if (texto === "") {
+        const mensaje = document.getElementById("mensajeError");
+        document.getElementById("resultado").value = "Escribe una frase antes de encriptar.";
+        mensaje.textContent = "Escribe una frase antes de encriptar.";
+        return;
+    }
+    try {
+        const resultado = CryptoJS.AES.encrypt(texto, clave).toString();
+        document.getElementById("resultado").value = resultado;
+    } catch (e) {
+        document.getElementById("resultado").value = "Error al encriptar";
+        mensaje.textContent = "Error al encriptar.";
+    }
 }
+
 
 function desencriptar() {
     const texto = document.getElementById("texto").value;
     try {
         const bytes = CryptoJS.AES.decrypt(texto, clave);
         const resultado = bytes.toString(CryptoJS.enc.Utf8);
-        document.getElementById("resultado").value = resultado || "⚠️ Texto inválido, intente escribiendo una frase válida";
+        const mensaje = document.getElementById("mensajeError");
+        mensaje.textContent = "";
+        document.getElementById("resultado").value = resultado || "Texto invalido";
+        mensaje.textContent = resultado ? "" : "Texto invalido";
     } catch (e) {
-        document.getElementById("resultado").value = "⚠️ Error al desencriptar, con una fráse común";
+        document.getElementById("resultado").value = "Error al desencriptar.";
+        mensaje.textContent = "Error al desencriptar.";
     }
-}
+};
 //archivo.txt
 window.addEventListener("DOMContentLoaded", function () {
   document.getElementById("fileInput").addEventListener("change", function () {
@@ -35,5 +52,19 @@ window.addEventListener("DOMContentLoaded", function () {
     }
   });
 });
+function borrarContenido() {
+    const textarea = document.getElementById("texto");
+    const resultado = document.getElementById("resultado");
+    if(textarea.value.trim() === "" && resultado.value.trim() === ""){
+      const mensaje = document.getElementById("mensajeError");
+      mensaje.textContent = "No hay nada que borrar";
+      return;
+    }
+    textarea.value = "";
+    resultado.value = "";
 
+    if (mensaje) {
+        mensaje.textContent = "";  
+    }
+}
 
